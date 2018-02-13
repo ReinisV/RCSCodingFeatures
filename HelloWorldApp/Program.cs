346cc69e51d102fe452f884d4cebe966274ac1e5
Reinis Vesers <reinis.vesers@accenture.com>
2018-02-13 21:36:14 +0200

    created my hello world app

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // izsaucam sevis izveidoto funkciju
+            // SayHello();
+            // izsaucam savu otro izveidoto funkciju
+            SayHelloToUser();
+        }
+
+        static void SayHelloToUser()
+        {
+            // palūdzam lietotājam ievadīt savu vārdu
+            // uzrakstot lūgumu ekrāna
+            Console.WriteLine("Please enter your name and age:");
+            // izveidojam mainīgo, kur glabāt lietotāja ievadīto vērtību
+            string usersName;
+            // paprasam konsolei ievadīt tekstu, saglabajām rezultātu mainīgajā
+            usersName = Console.ReadLine();
+            string age;
+            age = Console.ReadLine();
+            Console.WriteLine("Labdien," + usersName + "!" + "Tavs vecums ir " + age + "!");// Reinis "Labdien, Reinis!"
+            Console.ReadLine();
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadLine();
+        }
+    }
+}
