cea8965ba873104b6ee0437c7246110ada5b9727
Reinis Vesers <reinis.vesers@accenture.com>
2018-02-15 21:34:59 +0200

    started the new guessing game

---
+++ NumbersGuessingGame/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // paprasīt lietotājam, cik lielu skaitli viņš grib minēt
+            Console.WriteLine("lūdzu izvēlieties maksimālo skaitli, ko minēt");
+            int maxNumber = int.Parse(Console.ReadLine());
+
+            Random randomNumberMaker = new Random();
+            // uzģenerēt gadījuma skaitli līdz šai robežai
+            int guessableNumber = randomNumberMaker.Next(1, maxNumber);
+
+            // cikls: kamēr lietotājs neuzmin:
+            while (true)
+            {
+                // paprasīt lietotājam lai viņš min kāds skaitlis ir izveidots (iegūt ievadi)
+
+                // salīdzināt, vai lietotājs ir uzminējis
+                // // ja ir, tad izbeigt spēli
+                // ja nav uzminējis, tad pateikt lietotājam
+                // vai viņa minējums ir lielāks vai mazāks par minamo skaitli un turpināt spēli
+                if ()
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
