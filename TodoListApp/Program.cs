ceb35b4fc35ed64bafe58787e9677d30e19e25bf
Reinis Vesers <reinis.vesers@accenture.com>
2018-02-19 21:37:04 +0200

    implemented todo list saving and deleting from list

--- TodoListApp/Program.cs
+++ TodoListApp/Program.cs
@@ -13,13 +13,47 @@ namespace TodoListApp
             TodoList list = new TodoList();
             while (true)
             {
-                // pievienot jaunu darāmo lietu
-                Console.WriteLine("lūdzu ievadiet darāmo lietu:");
-                string todoTask = Console.ReadLine();
-                list.AddNewTodo(todoTask);
-                // dzēst visas darāmās lietas
-                // izvadīt visas darāmās lietas uz ekrāna
-                list.ShowAllTodos();
+                Console.WriteLine("lūdzu izvēlēties darbību:");
+                Console.WriteLine("a - pievienot");
+                Console.WriteLine("p - parādīt");
+                Console.WriteLine("d - dzēst");
+                Console.WriteLine("dv - dzēst visu");
+                string usersInput = Console.ReadLine();
+                switch (usersInput)
+                {
+                    case "a":
+                        // pievienot jaunu darāmo lietu
+                        Console.WriteLine("lūdzu ievadiet darāmo lietu:");
+                        string todoTask = Console.ReadLine();
+                        list.AddNewTodo(todoTask);
+                        break;
+                    case "p":
+                        // izvadīt visas darāmās lietas uz ekrāna
+                        list.ShowAllTodos();
+                        break;
+                    case "d":
+                        // dzēst konkrētu lietu
+                        Console.WriteLine("lūdzu ievadi dzēšamo numuru");
+                        list.ShowAllTodos();
+                        int index = int.Parse(Console.ReadLine());
+                        list.DeleteTodo(index - 1);
+                        break;
+                    case "dv":
+                        // lai ātri orientētos pa kodu,
+                        // iespējams uzspiest F12 uz funkcijas, lai uzreiz
+                        // dotos uz to
+                        // pievienot funkciju, kas notīra visu sarakstu
+                        list.DeleteAllTodos();
+                        break;
+                    case "s":
+                        // saglabājam saturu failā
+                        list.SaveToFile();
+                        break;
+                    case "l":
+                        // implementējam ielādi no faila izmantojot funkciju
+                       list.LoadFromFile();
+                        break;
+                }
             }
         }
     }
--- TodoListApp/TodoList.cs
+++ TodoListApp/TodoList.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace TodoListApp
 {
+    using System.IO;
 
     class TodoList
     {
@@ -24,6 +25,7 @@ namespace TodoListApp
             Console.WriteLine("uzdevums pievienots:" + task);
             todoEntries.Add(task);
         }
+        
 
         public void ShowAllTodos()
         {
@@ -31,12 +33,53 @@ namespace TodoListApp
             // skaititajs = skaititajs + 1; IR TAS PATS KAS skaititajs += 1;
             // IR TAS PATS KAS skaititajs++
             // i ir saīsinājums no vārda index, index latviešu valodā nozīmē skaitītājs
-            
+
             for (int i = 0; i < todoEntries.Count; i++)
             {
-                Console.WriteLine("your todo list entry is " + todoEntries[i]);
+                Console.WriteLine((i + 1) + ". " + todoEntries[i]);
+                Console.WriteLine();
             }
-            
+        }
+
+        public void DeleteTodo(int indexOfTodo)
+        {
+            // neļaut mēģināt izvilkt ierakstu no saraksta, 
+            // kura kārtas numurs ir ārpus ierakstu robežām
+            if (indexOfTodo >= this.todoEntries.Count)
+            {
+                // ja sarakstā ir 3 ieraksti, tad pēdējais indekss ir 2
+                Console.WriteLine("tāds ieraksts neeksitē");
+            }
+            else
+            {
+                todoEntries.RemoveAt(indexOfTodo);
+            }
+        }
+
+        public void DeleteAllTodos()
+        {
+            todoEntries.Clear();
+        }
+
+        public void SaveToFile()
+        {
+            // Ctrl + .
+            for(int i = 0; i < todoEntries.Count; i++)
+            {
+                // Append (angļu val) - Pievienot, papildināt
+                File.AppendAllText(
+                    @"C:\Users\reinis.vesers\Documents\TodoApplicationSettings\todos.txt",
+                    todoEntries[i] + "\r\n");
+            }
+        }
+
+        public void LoadFromFile()
+        {
+            string[] allLinesFromFile = File.ReadAllLines(@"C:\Users\reinis.vesers\Documents\TodoApplicationSettings\todos.txt");
+            foreach (string listEntry in allLinesFromFile)
+            {
+                todoEntries.Add(listEntry);
+            }
         }
     }
 }
