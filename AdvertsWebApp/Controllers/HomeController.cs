01c9d5a96cb5fbb83ff6e7915f3db0e043535a46
Reinis Vesers <reinis.vesers@accenture.com>
2018-02-22 21:28:42 +0200

    papildināju sludinājumu lapu ar iespēju paskatities visus sludinājumus un vienu atsevišķu sludinājumu detaļās

--- AdvertsWebApp/Controllers/HomeController.cs
+++ AdvertsWebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using AdvertsWebApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,54 @@ namespace AdvertsWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        public HomeController()
+        {
+            adverts = new List<Advert>();
+
+            Advert ad = new Advert();
+            ad.AdvertId = 1;
+            ad.Name = "bmw";
+            ad.AdvertText = "šis ir labs BMW";
+            ad.Price = 2000.95;
+            ad.CreationTime = DateTime.Now;
+
+            Advert homeAd = new Advert();
+            homeAd.AdvertId = 2;
+            homeAd.Name = "māja";
+            homeAd.AdvertText = "liela māja";
+            homeAd.Price = 12000;
+            homeAd.CreationTime = new DateTime(1999, 12, 31);
+
+            adverts.Add(ad);
+            adverts.Add(homeAd);
+        }
+
+        private List<Advert> adverts;
+
+        // šī funkcija tiek izsaukta, kad tiek pieprasīts weblapas bāzes ceļš
+        // GET: Home (piemēram www.ss.lv)
         public ActionResult Index()
         {
+            // izsaucam View() funkciju, lai uzģenerētu html rezultātu
+            // no mūsu Index.cshtml faila, tajā iekšā izmantojot datus,
+            // kas pieejami adverts sarakstā
+            return View(adverts);
+        }
+
+        public ActionResult Advert(int advertId)
+        {
+            // apskatām katru sludinājumu sludinājumu sarakstā
+            foreach(var ad in adverts)
+            {
+                // ja sludinājuma id ir tāds pats, kā tas, ko lietotājs pieprasījis
+                if(ad.AdvertId == advertId)
+                {
+                    // tad izveidojam skatu izmantojot šī sludinājuma datus
+                    // un atgriežam lietotājam
+                    return View(ad);
+                }
+            }
+
             return View();
         }
     }
